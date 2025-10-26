a498f2c4347312618b6166e924075cdb9e8d28a0
nar1nari <nar1nariq@proton.me>
2025-10-26 19:10:07 +0500

    Add Github Actions (and fix music typo)

--- src/music/mod.rs
+++ src/music/mod.rs
@@ -19,14 +19,14 @@ impl Music {
 }
 
 const ALL_MUSIC: [Music; 4] = [
-Music::new("sunny butter", "sunny_butter.wav",
+Music::new("sunny butter", "sunny_butter.mp3",
     "This was the first track I ever made.
     It was supposed to sound calm and gentle, but after a few listens I realized that I used the same melody as Sayo-nara from DDLC 
     - so it kinda ended up with a darker vibe :_)"),
-Music::new("coconut soup", "coconut_soup.wav", 
+Music::new("coconut soup", "coconut_soup.mp3", 
 "This melody has kind of a beach vibe - maybe it could even work as a soundtrack for some game."),
-Music::new("papa's secret technique", "papas_secret_technique.wav", 
+Music::new("papa's secret technique", "papas_secret_technique.mp3", 
 "A battle track inspired by Mortal Kombat and LISA: The Painful."),
-Music::new("auspicious hamlet", "auspicious_hamlet.wav", 
+Music::new("auspicious hamlet", "auspicious_hamlet.mp3", 
 "A Zelda-like soundtrack for a humble village.")
 ];
