f42f547b0c4dca43ad933479816ee3b041729374
nar1nari <nar1nariq@proton.me>
2026-01-31 19:56:27 +0500

    Update cache

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use leptos::prelude::*;
 use nar1nari_space::App;
 
 fn main() {
-    // set up logging
     _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
 
